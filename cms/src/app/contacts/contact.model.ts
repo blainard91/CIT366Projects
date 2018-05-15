f99213e0f1056465993bf04f101f5e1023e6ac95
blainard91 <blaine.akers@gmail.com>
2018-05-15 10:27:36 -0600

    Project1

---
+++ cms/src/app/contacts/contact.model.ts
@@ -0,0 +1,15 @@
+export class Contact {
+  public contactId: number;
+  public name: string;
+  public email: string;
+  public phone: string;
+  public imageUrl: string;
+
+  constructor(contactId: number, name: string, email: string, phone: string, imageUrl: string) {
+    this.contactId = contactId;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.imageUrl = imageUrl;
+  }
+}
