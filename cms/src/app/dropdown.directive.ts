6c64503e59142b2f475528b81a7e3da2c972ec0f
blainard91 <blaine.akers@gmail.com>
2018-05-31 10:45:36 -0600

    click not mouseover

--- cms/src/app/dropdown.directive.ts
+++ cms/src/app/dropdown.directive.ts
@@ -6,7 +6,7 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener ('mouseenter') mouseover() {
+  @HostListener ('click') mouseover() {
     this.isOpen = !this.isOpen;
   }
 
